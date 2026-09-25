753d8ce7663c328030796d149dea7c43bf87197e
Julien67890 <135655358+Julien67890@users.noreply.github.com>
2026-09-25 11:13:50 +0200

    Update cache version from v9 to v12

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ascae-ci-v9';
+const CACHE = 'ascae-ci-v12';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './logo.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', e => {
